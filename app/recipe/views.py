d96162043cb2af712fc44584feb6299c73f2625a
datny <robert.przydatek.cl@gmail.com>
2023-11-02 20:42:27 +0100

    ADD: main part of recipe api fin

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -16,3 +16,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated User"""
         return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
